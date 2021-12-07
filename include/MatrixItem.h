63f4c5225ee66b18dbd4f3d00cb985b85f5f28ba
Netanel Stern <netanel@ezsave.co.il>
2021-12-07 15:14:34 +0200

    add basic classes

--- include/MatrixItem.h
+++ include/MatrixItem.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
+#include "Square.h"
 
 class MatrixItem {
 
 public:
+    MatrixItem();
+
+    virtual void draw(sf::RenderWindow& window);
+
+protected:
+    Square m_square;
 
 private:
 
--- include/Menu.h
+++ include/Menu.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "Matrix.h"
 
 class Menu : public Matrix {
 
 public:
-
+	Menu(const unsigned int col);
+	~Menu();
 private:
-
+	
 };
