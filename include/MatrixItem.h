8039903b6f7a9d21a8c22238dddd6a1ded2c42b2
Netanel Stern <netanel@ezsave.co.il>
2021-12-07 22:09:30 +0200

    matrix item

--- include/MatrixItem.h
+++ include/MatrixItem.h
@@ -7,13 +7,18 @@
 class MatrixItem {
 
 public:
-    MatrixItem();
+	//todo:: is the origin require as ctor params?
+    MatrixItem(int row = 0, int col = 0, float originX = 0, float originY = 0);
 
     virtual void draw(sf::RenderWindow& window);
+    void setOrigin(const sf::Vector2f& origin);
 
 protected:
     Square m_square;
 
-private:
+    int m_row;
+    int m_col;
 
+private:
+	
 };
